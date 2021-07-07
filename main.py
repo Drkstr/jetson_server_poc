7ccf176ee1b62c9d19d113dc30bc95afd2543f7c
rakshak <r.rakshak@gmail.com>
2021-07-07 20:58:07 +1000

    fixed method name

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 @app.route('/health')
 def health():
     return 'All systems are go!'
-    pass
 
 
 @app.route('/readJson')
 def read_json():
-    return json.loads("test.json")
+    json_file = open("test.json",)
+    return json.loads(json_file)
 
 
 if __name__ == '__main__':

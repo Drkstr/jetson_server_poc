3ba8483d3e9135bb39cae6fccd208a8ee4802baa
Rakshak Raghavan <r.rakshak@gmail.com>
2021-09-01 23:52:24 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,41 @@ import json
 import flask
 from flask import Flask
 from flask import send_file
+from flask import jsonify, make_response
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def name():
-    return 'Jetson 1'
-
+    response = flask.Response("Jeton 1")
+    return response
 
 @app.route('/health')
 def health():
-    return 'All systems are go!'
+    response = flask.Response("All systems are go!")
+    return response
 
 
 @app.route('/getJson')
 def read_json():
     json_file = open("test.json", )
-    return flask.jsonify(json.loads(json_file.read()))
+    # return flask.jsonify(json.loads(json_file.read()))
+    return make_response(jsonify(json.loads(json_file.read())))
 
 
 @app.route('/getImage')
 def get_image():
-    return send_file("test_image.jpeg", mimetype='image/gif')
+    return make_response(send_file("test_image.jpeg", mimetype='image/gif'))
+
+
+@app.after_request
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
+
